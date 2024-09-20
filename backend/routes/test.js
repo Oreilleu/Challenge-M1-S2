f98185effaf562fef07c558fb3a16eb2abf6d202
Oreilleu <aurelien.picardpabijan@gmail.com>
2024-09-20 13:50:37 +0200

    example route test

--- backend/routes/test.js
+++ backend/routes/test.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const testController = require("../controllers/test");
 
-router.get("/test", testController.testController);
+router.get("/", testController.testController);
 
 module.exports = router;
