fdbab46f01293a848b901f282195cb1a0de0e91f
Oreilleu <aurelien.picardpabijan@gmail.com>
2024-09-20 09:39:11 +0200

    init backend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,1 +1,10 @@
-<template>Challenge MA S2</template>
+<template>
+  <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
+</template>
