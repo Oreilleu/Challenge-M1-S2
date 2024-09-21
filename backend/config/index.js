8bab9bf1c9c8214dd67565ea41e7e6967cc3e29b
Oreilleu <aurelien.picardpabijan@gmail.com>
2024-09-21 10:13:36 +0200

    fix: connexion string

--- backend/config/index.js
+++ backend/config/index.js
@@ -3,9 +3,10 @@ require("dotenv").config();
 module.exports = {
   port: process.env.PORT || 3000,
   db: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    name: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
+    user: process.env.DB_USER || "",
+    host: process.env.DB_HOST || "",
+    port: process.env.DB_PORT || "",
+    name: process.env.DB_NAME || "",
+    password: process.env.DB_PASSWORD || "",
   },
 };
--- backend/utils/connectDb.js
+++ backend/utils/connectDb.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 const config = require("../config");
 
 const connectDb = async () => {
+  const { user, password, host, name } = config.db;
+  const connexionString = `mongodb+srv://${user}:${password}@${host}/?retryWrites=true&w=majority&appName=${name}`;
+
   try {
-    await mongoose.connect(
-      `mongodb+srv://${config.db.host}:${config.db.password}@${config.db.name}.u9xjc.mongodb.net/?retryWrites=true&w=majority&appName=${config.db.name}`
-    );
+    await mongoose.connect(connexionString);
     console.log("Connexion réussie à la base données");
   } catch (error) {
     console.error("Erreur lors de la connexion à la base de données : ", error);
