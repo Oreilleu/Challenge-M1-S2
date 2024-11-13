5f31ad86ce204aaf40c1fba97cda0ca7730983be
Oreilleu <aurelien.picardpabijan@gmail.com>
2024-11-13 12:33:50 +0100

    refact: remove tailwind css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,21 @@
 <script setup lang="ts">
-import { RouterLink, RouterView, useRoute, useRouter } from 'vue-router'
-import { isAuthenticated } from '@/utils/isAuthenticatedUser'
-import localStorageHandler from './utils/localStorageHandler'
-import { onMounted, ref } from 'vue'
-import { LocalStorageKeys } from './utils/types/local-storage-keys.enum'
+import { RouterView } from 'vue-router'
+import { onMounted } from 'vue'
 import useAuthStore from './utils/store/useAuthStore'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
 const authStore = useAuthStore()
 
 onMounted(() => {
   authStore.checkAuthentication()
 })
-
-const route = useRoute()
 </script>
 
 <template>
-  <header>
-    <HeaderComponent />
-  </header>
+  <Header />
   <main class="min-h-screen">
     <RouterView />
   </main>
-  <FooterComponent />
+  <Footer />
 </template>
