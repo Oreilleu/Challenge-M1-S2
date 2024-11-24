795861d88adeab67e4791b79336c0109d7b22cc8
Salim-T <tizi_salim@yahoo.fr>
2024-11-24 15:55:03 +0100

    change images folder path

--- backend/src/middleware/storage.ts
+++ backend/src/middleware/storage.ts
@@ -2,7 +2,7 @@ import multer from "multer";
 
 export const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/images/product");
+    cb(null, "public/images");
   },
   filename: function (req, file, cb) {
     const extention = `.${file.mimetype.split("/")[1]}`;
