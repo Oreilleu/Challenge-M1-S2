f7e94d279be45c06e1627acb0936a619e88a6cf6
Salim-T <tizi_salim@yahoo.fr>
2025-01-06 21:38:28 +0100

    make update status method for orders backend

--- backend/src/routes/order.ts
+++ backend/src/routes/order.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { create, getAll, getOne, remove, update, getPaginatedOrders, deleteOrder } from "../controllers/order";
+import { create, getAll, getOne, update, getPaginatedOrders, deleteOrder, getOrderById, updateStatusById} from "../controllers/order";
 import checkAdmin from "../middleware/checkAdmin";
 
 const orderRouter = express.Router();
@@ -8,8 +8,10 @@ orderRouter.post("/create", create);
 orderRouter.get("/all", getAll);
 orderRouter.get("/one/:id", getOne);
 orderRouter.put("/update/:id", update);
-// orderRouter.delete("/delete/:id", remove);
 orderRouter.get("/paginated-orders", checkAdmin, getPaginatedOrders);
 orderRouter.delete("/delete/:id", checkAdmin, deleteOrder);
+orderRouter.get("/:id", checkAdmin, getOrderById);
+orderRouter.put("/update-status/:id", checkAdmin, updateStatusById);
+
 
 export default orderRouter;
